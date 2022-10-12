bcf599394634e3e8b5c74bb76d67990e7ce61972
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-12 16:52:58 +0100

    progress on making calculations work pretty well

--- script.js
+++ script.js
@@ -5,21 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
     let backspace = document.querySelector('.backspace') 
     let current = document.querySelector('.current')
     let ongoing = document.querySelector('.ongoing')    
+    let small = ""
     let array = []
+    let opperands = ["x", "/", "+", "-"]
 
     buttons.forEach(button=>{
         button.addEventListener('click', () => {
             // do something with the button
-            console.log(button.firstElementChild.textContent)
-            if(button.firstElementChild.textContent != "=" ){
-                current.textContent = button.firstElementChild.textContent
-                array.push(button.firstElementChild.textContent)
-                if(array.length == 3){
-                    ongoing.innerHTML = caluclate(array)
+            clicked = button.firstElementChild.textContent
+
+            if(clicked == "=" && array.length == 2){
+                array.push(small)
+                total = caluclate(array)
+                ongoing.innerHTML = total
+                array = []
+                small = ""
+                array.push(total)
+                console.log(array)
+            }
+            else if(clicked == "="){}
+            else if(opperands.includes(clicked)){
+                current.textContent = clicked
+                if(small.length > 0){
+                    array.push(small)
+                    small = ""
+                }
+                if(array.length == 1){
+                    array.push(clicked)
                 }
             }
-            else{
-                ongoing.innerHTML = caluclate(array)
+            else{ 
+                small += clicked
+                current.textContent = small
+                console.log(small)
             }
         })
     })
@@ -27,23 +45,19 @@ document.addEventListener("DOMContentLoaded", () => {
     // clear history and reset scren
     clear.addEventListener("click", () =>{
         array.length = 0
-        current.textContent = " "
-        ongoing.textContent = " "
+        small = ""
+        console.log(array, small)
     })
 
     backspace.addEventListener("click", () =>{
         // do something on clear button
-        array.pop()
-        console.log(array)
-        current.textContent = array[array.length - 1]
+       
     })
 
     function caluclate(array){
         a = parseInt(array[0])
         b = parseInt(array[2])
         opperand = array[1]
-        console.log(array)
-        array.length = 0
 
         if(opperand == "x"){
             array.push(a * b)
