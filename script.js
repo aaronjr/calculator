88814099d75bf28270cb36d9708ae0116fcb6364
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-10-12 16:09:57 +0100

    add clear function and make start on calculations

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let clear = document.querySelector('.clear')
     let backspace = document.querySelector('.backspace') 
     let current = document.querySelector('.current')
-    
+    let ongoing = document.querySelector('.ongoing')    
     let array = []
 
     buttons.forEach(button=>{
@@ -14,24 +14,53 @@ document.addEventListener("DOMContentLoaded", () => {
             if(button.firstElementChild.textContent != "=" ){
                 current.textContent = button.firstElementChild.textContent
                 array.push(button.firstElementChild.textContent)
+                if(array.length == 3){
+                    ongoing.innerHTML = caluclate(array)
+                }
             }
             else{
-                caluclate(array)
+                ongoing.innerHTML = caluclate(array)
             }
         })
     })
 
+    // clear history and reset scren
     clear.addEventListener("click", () =>{
-        // do something on clear button
-        array = []
+        array.length = 0
+        current.textContent = " "
+        ongoing.textContent = " "
     })
 
     backspace.addEventListener("click", () =>{
         // do something on clear button
+        array.pop()
+        console.log(array)
+        current.textContent = array[array.length - 1]
     })
 
     function caluclate(array){
-        console.log(array.join(' '))
+        a = parseInt(array[0])
+        b = parseInt(array[2])
+        opperand = array[1]
+        console.log(array)
+        array.length = 0
+
+        if(opperand == "x"){
+            array.push(a * b)
+            return a * b
+        }
+        else if(opperand == "/"){
+            array.push(a / b)
+            return a / b
+        }
+        else if(opperand == "-"){
+            array.push(a - b)
+            return a - b
+        }
+        else{
+            array.push(a + b)
+            return a + b
+        }
     }
 
 
